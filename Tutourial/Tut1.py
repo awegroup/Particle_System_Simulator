170a789dd9af0a5c05e044bb5953ec1c0d0fdddb
Markk116 <29547434+Markk116@users.noreply.github.com>
2023-10-31 09:39:06 +0100

    Merging changes after fork

---
+++ Tutourial/Tut1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 15:17:42 2023
+
+@author: Mark
+"""
+
+import numpy as np
+import saddle_form_input as input
+import matplotlib.pyplot as plt
+import pandas as pd
+from Msc_Alexander_Batchelor.src.particleSystem.ParticleSystem import ParticleSystem
